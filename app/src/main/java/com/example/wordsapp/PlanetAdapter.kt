4fa985d8209db9eb1fdda7ee5399c0b9f9f9f7fe
aaiovinoo <abigail.iovino@gmail.com>
2022-02-18 15:43:13 -0600

    add rest of files

---
+++ app/src/main/java/com/example/wordsapp/PlanetAdapter.kt
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.wordsapp
+
+import android.content.Intent
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.accessibility.AccessibilityNodeInfo
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Adapter for the [RecyclerView] in [MainActivity].
+ */
+class PlanetAdapter :
+    RecyclerView.Adapter<PlanetAdapter.LetterViewHolder>() {
+
+    private val list = listOf("Sun","Mercury","Venus", "Earth", "Mars","Jupiter","Saturn",
+    "Uranus","Neptune","Pluto")
+
+
+    /**
+     * Provides a reference for the views needed to display items in your list.
+     */
+    class LetterViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val button = view.findViewById<Button>(R.id.button_item)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    /**
+     * Creates new views with R.layout.item_view as its template
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LetterViewHolder {
+        val layout = LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.item_view, parent, false)
+        // Setup custom accessibility delegate to set the text read
+        layout.accessibilityDelegate = Accessibility
+        return LetterViewHolder(layout)
+    }
+
+    /**
+     * Replaces the content of an existing view with new data
+     */
+    override fun onBindViewHolder(holder: LetterViewHolder, position: Int) {
+        val item = list.get(position)
+        holder.button.text = item.toString()
+        holder.button.setOnClickListener {
+            val context = holder.view.context
+            // The name of the activity you want to show is specified with DetailActivity::class.java. An actual DetailActivity object is created behind the scenes.
+            val intent = Intent(context, DetailActivity::class.java)
+            // n extra is a piece of data, such as a number or string, that is given a name to be retrieved later.
+            intent.putExtra(DetailActivity.PLANET, holder.button.text.toString())
+            context.startActivity(intent)
+        }
+    }
+
+    // Setup custom accessibility delegate to set the text read with
+    // an accessibility service
+    companion object Accessibility : View.AccessibilityDelegate() {
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun onInitializeAccessibilityNodeInfo(
+            host: View?,
+            info: AccessibilityNodeInfo?
+        ) {
+            super.onInitializeAccessibilityNodeInfo(host, info)
+            // With `null` as the second argument to [AccessibilityAction], the
+            // accessibility service announces "double tap to activate".
+            // If a custom string is provided,
+            // it announces "double tap to <custom string>".
+            val customString = host?.context?.getString(R.string.look_up_words)
+            val customClick =
+                AccessibilityNodeInfo.AccessibilityAction(
+                    AccessibilityNodeInfo.ACTION_CLICK,
+                    customString
+                )
+            info?.addAction(customClick)
+        }
+    }
+}
